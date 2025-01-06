39f23c38fa78fb396cbb5c9a7f1dbe2adeb320e2
James Farrell <jamesgjfarrell@gmail.com>
2025-01-06 19:10:52 +0000

    added Congratulations.tsx with component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Congratulations from './Congratulations'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,6 +35,7 @@ function App() {
             <li><code>npm run preview</code></li>
         </ol>
       </div>
+      <Congratulations />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/Congratulations.tsx
@@ -0,0 +1,13 @@
+export default function Congratulations(){
+    return (
+        <>
+            <h1>Congratulations!</h1>
+            <p>
+                You have successfully created a new Vite + React project.
+            </p>
+            <p>
+                You can now start building your application.
+            </p>
+        </>
+    )
+}
